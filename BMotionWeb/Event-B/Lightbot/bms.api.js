aaf723fc13424718dd66a5a034e3894389a43f7a
Lukas Ladenberger <lukas.ladenberger@googlemail.com>
2016-11-06 23:13:06 +0100

    remove

--- BMotionWeb/Event-B/Lightbot/bms.api.js
+++
@@ -1,7 +0,0 @@
-var bms = bms || {};
-var angular = angular || {};
-
-(function() {
-  bms = window.parent.bmsapi(window.frameElement.getAttribute('sessionId'), window.frameElement.getAttribute('viewId'));
-  angular = window.parent.angular;
-})();
