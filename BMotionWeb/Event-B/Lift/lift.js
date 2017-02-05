89dde3300d9c5f040f9653530e6b3b86dde8cec4
Lukas Ladenberger <lukas.ladenberger@gmail.com>
2017-02-05 17:00:01 +0100

    Adapted Lift case study

--- BMotionWeb/Event-B/Lift/lift.js
+++ BMotionWeb/Event-B/Lift/lift.js
@@ -15,3 +15,55 @@ bms.observe('formula', {
     origin.text('Current Floor: ' + values[0]);
   }
 });
+
+bms.observe('formula', {
+  selector: "#txt_direction",
+  formulas: [
+    "move"
+  ],
+  trigger: function(origin, values) {
+    origin.text('Moving: ' + values[0]);
+  }
+});
+
+bms.observe('formula', {
+  selector: "#txt_direction",
+  formulas: [
+    "move"
+  ],
+  trigger: function(origin, values) {
+    origin.text('Moving: ' + values[0]);
+  }
+});
+
+bms.observe("formula", {
+  selector: "#lift",
+  formulas: ["floor"],
+  translate: true,
+  trigger: function(origin, values) {
+    var door = origin.find("#door");
+    switch (values[0]) {
+      case 2:
+        door.attr("y", "20");
+        break
+      case 1:
+        door.attr("y", "140");
+        break
+      case 0:
+        door.attr("y", "250");
+        break
+    }
+  }
+});
+
+bms.observe("formula", {
+  selector: "#door",
+  formulas: ["door"],
+  trigger: function(origin, values) {
+    if (values[0] === 'open') {
+      origin.attr("fill", "white");
+    } else if (values[0] === 'closed') {
+      origin.attr("fill", "gray");
+    }
+  }
+});
